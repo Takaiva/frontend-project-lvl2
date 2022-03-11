63e7644bffb53d755dead4de355033ac2bcb0039
Александр Сдобников <sdobnikove@mail.ru>
2022-03-11 17:15:20 +0300

    add gendiff info

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compare two configuration files and shows a difference.')
+  .version('0.0.1');
+
+program.parse();
