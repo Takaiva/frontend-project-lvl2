c86f44bde4d603b66733ee0d8130f20bb04dcbfd
Александр Сдобников <sdobnikove@mail.ru>
2022-03-15 00:57:21 +0300

    add getDataOfDiff.js

--- src/getDataOfDiff.js
+++ src/getDataOfDiff.js
@@ -1,16 +1,47 @@
 import _ from 'lodash';
 
+const indexOfNestedArr = (coll) => {
+  const result = coll
+    .map((item) => {
+      if (item.length !== 2) {
+        return undefined;
+      }
+      return coll.indexOf(item);
+    })
+    .filter((item) => item !== undefined);
+  return result[0];
+};
+
+const normalizeData = (coll) => {
+  const result = coll
+    .reduce((acc, item) => {
+      if (coll.indexOf(item) === indexOfNestedArr(coll)) {
+        const file1Data = item[0];
+        const file2Data = item[1];
+        acc.push(file1Data);
+        acc.push(file2Data);
+      }
+      acc.push(item);
+      return acc;
+    }, [])
+    .filter((item) => item.length > 2);
+  return result;
+};
+
 const getDataOfDiff = (file1, file2) => {
   const allKeys = _.union(Object.keys(file1), Object.keys(file2));
   const sortedAllKeys = _.sortBy(allKeys);
-  const diff = sortedAllKeys.map((key) => {
+  const diffData = sortedAllKeys.map((key) => {
     const value1 = file1[key];
     const value2 = file2[key];
     if (value1 === value2) {
       return [key, value1, 'bothHaveTheSame'];
     }
     if (value1 !== value2 && value1 !== undefined && value2 !== undefined) {
-      return (([key, value1, 'hasFirstFile']), ([key, value2, 'hasSecondFile']));
+      return [
+        [key, value1, 'hasFirstfile'],
+        [key, value2, 'hasSecondFile'],
+      ];
     }
     if (value1 !== value2 && value2 === undefined) {
       return [key, value1, 'hasOnlyFirstFile'];
@@ -18,8 +49,9 @@ const getDataOfDiff = (file1, file2) => {
     if (value1 !== value2 && value1 === undefined) {
       return [key, value2, 'hasOnlySecondFile'];
     }
-    return diff;
+    return diffData;
   });
+  return normalizeData(diffData);
 };
 
 export default getDataOfDiff;
