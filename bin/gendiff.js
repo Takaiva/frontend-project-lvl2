51f8b2f8419a9fb9f118cb508809d8d899a0e643
Александр Сдобников <sdobnikove@mail.ru>
2022-03-15 05:34:46 +0300

    add action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import genDiff from '../src/genDiff.js';
 
 program
   .name('gendiff')
   .description('Compare two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
