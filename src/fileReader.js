b1327072d2672e5378c9bbac006c726919ff5fbd
Александр Сдобников <sdobnikove@mail.ru>
2022-03-15 04:51:17 +0300

    add fileReader.js

---
+++ src/fileReader.js
@@ -0,0 +1,8 @@
+import path from 'path';
+import { readFileSync } from 'fs';
+
+const getPath = (file) => path.resolve('..', '__fixtures__', file);
+const getExt = (file) => path.extname(file);
+const readFile = (file) => readFileSync(getPath(file), 'utf-8');
+
+export { getPath, getExt, readFile };
