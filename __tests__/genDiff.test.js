e692b2cefc4d022fbab892a3c17908a146a8d2f7
Александр Сдобников <sdobnikove@mail.ru>
2022-03-17 04:40:46 +0300

    update

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import json from '../__fixtures__/json.js';
+import genDiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const json1 = getFixturePath('file1.json');
+const json2 = getFixturePath('file2.json');
+
+test('check resulted diff', () => {
+  const result = genDiff(json1, json2, 'json');
+  expect(result).toBe(json);
+});
