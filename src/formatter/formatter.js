58590a1fb19b00a798545929aafefe268d5356c6
Александр Сдобников <sdobnikove@mail.ru>
2022-03-25 02:53:43 +0300

    update

--- src/formatter/formatter.js
+++ src/formatter/formatter.js
@@ -1,8 +1,8 @@
 import getStylish from './stylish.js';
 
-const getFormatted = (data, format) => {
+const getFormatted = (data, format = 'stylish') => {
   if (format === 'stylish') {
-    return getStylish(data, format);
+    return getStylish(data);
   }
   throw new Error('Falsy format');
 };
