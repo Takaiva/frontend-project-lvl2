76edc842273fed516bc7987075b3d3362a3683cc
Александр Сдобников <sdobnikove@mail.ru>
2022-03-27 22:35:26 +0300

    update

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -12,8 +12,8 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const json1 = getFixturePath('file1.json');
 const json2 = getFixturePath('file2.json');
-const yaml1 = getFixturePath('file1.yaml');
-const yaml2 = getFixturePath('file2.yaml');
+const yaml1 = getFixturePath('file1.yml');
+const yaml2 = getFixturePath('file2.yml');
 
 describe('check stylish output', () => {
   test('check resulted diff with json files', () => {
